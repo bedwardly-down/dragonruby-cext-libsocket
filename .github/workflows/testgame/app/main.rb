c0ff298d5155d040c589431cc3c17ee4c5c539bf
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-04-10 03:00:48 +0900

    Implement tests (failing since commented out)

---
+++ .github/workflows/testgame/app/main.rb
@@ -0,0 +1,12 @@
+# No functionality yet, so require is commented out for now.
+# require 'libsocket/require.rb'
+
+def tick(args)
+  # $gtk.ffi_misc.gtk_dlopen('libsocket')
+  if FFI.const_defined?(:SOCKET)
+    $gtk.write_file('success', 'success')
+  else
+    puts 'Failed to load libsocket'
+  end
+  $gtk.request_quit
+end
