e78571363dea4bc02e45c0143c4caa7085cb618f
Brandon Dulaney <social@brandongrows.me>
2023-04-09 22:54:42 -0500

    Windows build and test improvements

---
+++ .github/workflows/run_tests.ps1
@@ -0,0 +1,11 @@
+mkdir ${{ env.TESTGAME_DIR }}\libsocket
+copy *.rb ${{ env.TESTGAME_DIR }}
+
+$env:SDL_VIDEODRIVER=dummy $env:SDL_AUDIODRIVER=dummy .\dragonruby.exe ${{ env.TESTGAME_DIR }}
+
+if ( Test-Path -Path ${{ env.TESTGAME_DIR\success }} -PathType Leaf )
+  Write-Output "Tests finished successfully."
+  exit 0
+else
+  Write-Output "Tests failed."
+  exit 1
