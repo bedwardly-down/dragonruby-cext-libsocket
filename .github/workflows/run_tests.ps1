5f8f9b22fd9463ed7f722dd159f1214f309acbf7
Brandon Dulaney <social@brandongrows.me>
2023-04-10 02:16:35 -0500

    Windows powershell build attempt 13

--- .github/workflows/run_tests.ps1
+++ .github/workflows/run_tests.ps1
@@ -1,11 +1,10 @@
-mkdir ${{ env.TESTGAME_DIR }}\libsocket
-copy *.rb ${{ env.TESTGAME_DIR }}
+mkdir '${{ env.TESTGAME_DIR }}\libsocket'
+copy *.rb '${{ env.TESTGAME_DIR }}'
 
-$env:SDL_VIDEODRIVER=dummy $env:SDL_AUDIODRIVER=dummy .\dragonruby.exe ${{ env.TESTGAME_DIR }}
+$env:SDL_VIDEODRIVER=dummy $env:SDL_AUDIODRIVER=dummy .\dragonruby.exe '${{ env.TESTGAME_DIR }}'
 
-if ( Test-Path -Path ${{ env.TESTGAME_DIR\success }} -PathType Leaf )
+if ( Test-Path -Path '${{ env.TESTGAME_DIR }}\success' -PathType Leaf )
   Write-Output "Tests finished successfully."
   exit 0
 else
   Write-Output "Tests failed."
-  exit 1
