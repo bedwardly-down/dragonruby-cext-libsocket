16f9d120172db0da63e1ee7e49f8cb2cdab4502c
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-04-11 17:08:39 +0900

    Use dr_platform in download script too

--- .github/workflows/download_dr_for_ci.sh
+++ .github/workflows/download_dr_for_ci.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 version=$1
-platform=$2
+platform=$(./scripts/dr_platform.sh)
 zip_file="dragonruby-for-ci-$version-pro-$platform.zip"
 download_url="https://github.com/kfischer-okarin/dragonruby-for-ci/releases/download/$version/$zip_file"
 
