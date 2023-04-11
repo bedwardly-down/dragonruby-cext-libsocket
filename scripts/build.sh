c6f00c7ee69f7a0e9f2a5b8ca7ab87a134c09905
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-04-11 17:05:57 +0900

    Extract build.sh script

---
+++ scripts/build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e # Exit immediately if any command fails
+
+target_dir=$1
+platform=$(./scripts/dr_platform.sh)
+source_entrypoint=new-libsocket-bind.c
+
+mkdir -p $target_dir
+
+case $platform in
+  linux-amd64)
+    clang -shared \
+          -isystem include -I . -fPIC \
+          -o $target_dir/libsocket.so \
+          $source_entrypoint;;
+  macos)
+    clang -shared \
+          -arch x86_64 -arch arm64 \
+          -isystem include -I . -fPIC \
+          -o $target_dir/libsocket.dylib \
+          $source_entrypoint;;
+  windows-amd64)
+    clang -shared \
+          --sysroot=/C/ProgramData/chocolatey/lib/mingw/tools/install/mingw64 \
+          --target=x86_64-w64-mingw32 \
+          -fuse-ld=lld \
+          -isystem include -I . -lws2_32 \
+          -o $target_dir/libsocket.dll \
+          $source_entrypoint;;
+esac # No default case needed since dr_platform.sh already fails on unsupported platforms
