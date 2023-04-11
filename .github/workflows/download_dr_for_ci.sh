8fa263e63c205ed79db860ef3f9c2f498fef2c1c
Brandon Dulaney <social@brandongrows.me>
2023-04-11 13:41:33 -0500

    Improve scripts for local usage

--- .github/workflows/download_dr_for_ci.sh
+++ .github/workflows/download_dr_for_ci.sh
@@ -6,5 +6,5 @@ zip_file="dragonruby-for-ci-$version-pro-$platform.zip"
 download_url="https://github.com/kfischer-okarin/dragonruby-for-ci/releases/download/$version/$zip_file"
 
 curl -L -O $download_url
-unzip $zip_file
-chmod u+x ./dragonruby
+unzip -d dragonruby $zip_file
+chmod u+x ./dragonruby/dragonruby
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,21 +3,27 @@
 set -e # Exit immediately if any command fails
 
 target_dir=$1
+target_ver=4.7
 platform=$(./scripts/dr_platform.sh)
 source_entrypoint=new-libsocket-bind.c
+
+# Check if dragonruby folder exists; makes this useful locally too
+if [ ! -d "./dragonruby" ]; then
+  sh .github/workflows/download_dr_for_ci.sh $target_ver
+fi
 
 mkdir -p $target_dir
 
 case $platform in
   linux-amd64)
     clang -shared \
-          -isystem include -I . -fPIC \
+          -isystem ./dragonruby/include -I . -fPIC \
           -o $target_dir/libsocket.so \
           $source_entrypoint;;
   macos)
     clang -shared \
           -arch x86_64 -arch arm64 \
-          -isystem include -I . -fPIC \
+          -isystem ./dragonruby/include -I . -fPIC \
           -o $target_dir/libsocket.dylib \
           $source_entrypoint;;
   windows-amd64)
@@ -25,7 +31,7 @@ case $platform in
           --sysroot=/C/ProgramData/chocolatey/lib/mingw/tools/install/mingw64 \
           --target=x86_64-w64-mingw32 \
           -fuse-ld=lld \
-          -isystem include -I . -lws2_32 \
+          -isystem ./dragonruby/include -I . -lws2_32 \
           -o $target_dir/libsocket.dll \
           $source_entrypoint;;
 esac # No default case needed since dr_platform.sh already fails on unsupported platforms
