976242dae44f30438424c09f7f201794a11380b2
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-04-12 09:22:37 +0900

    Allow specifying dragon ruby headers

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,23 +1,30 @@
 #!/bin/sh
 
+# Usage:
+# ./scripts/build.sh <target_dir> [<dr_headers>]
+#
+#   <target_dir> is the directory where the compiled extension will be written to
+#   <dr_headers> is the directory containing the DragonRuby headers (default: .)
+
 set -e # Exit immediately if any command fails
 
 target_dir=$1
 platform=$(./scripts/dr_platform.sh)
 source_entrypoint=new-libsocket-bind.c
+dr_headers=${2:-.}
 
 mkdir -p $target_dir
 
 case $platform in
   linux-amd64)
     clang -shared \
-          -isystem include -I . -fPIC \
+          -isystem include -I $dr_headers -fPIC \
           -o $target_dir/libsocket.so \
           $source_entrypoint;;
   macos)
     clang -shared \
           -arch x86_64 -arch arm64 \
-          -isystem include -I . -fPIC \
+          -isystem include -I $dr_headers -fPIC \
           -o $target_dir/libsocket.dylib \
           $source_entrypoint;;
   windows-amd64)
@@ -30,7 +37,7 @@ case $platform in
           --sysroot=$SYSROOT \
           --target=x86_64-w64-mingw32 \
           -fuse-ld=lld \
-          -isystem include -I . -lws2_32 \
+          -isystem include -I $dr_headers -lws2_32 \
           -o $target_dir/libsocket.dll \
           $source_entrypoint;;
 esac # No default case needed since dr_platform.sh already fails on unsupported platforms
