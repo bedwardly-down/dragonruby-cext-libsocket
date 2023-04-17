1945fd05c37e9c361e30c4fc6fdaa669a70c64dc
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-04-17 11:44:30 +0900

    Improve Test TCP server logging

--- .github/workflows/test-tcp-server.rb
+++ .github/workflows/test-tcp-server.rb
@@ -1,9 +1,22 @@
+require 'logger'
 require 'socket'
 
+logger = Logger.new(STDOUT)
+logger.formatter = proc do |_severity, datetime, _progname, msg|
+  time = datetime.strftime('%Y-%m-%d %H:%M:%S')
+  "[Test TCP Server] #{time} -- #{msg}\n"
+end
+
 server = TCPServer.new(7777)
+logger.info 'Listening on port 7777'
 
 client = server.accept
+logger.info 'Client connected'
 request = client.gets
-puts request
+logger.info request
 client.puts 'Hello from Ruby!'
 client.close
+logger.info 'Client disconnected'
+
+server.close
+logger.info 'Server closed'
