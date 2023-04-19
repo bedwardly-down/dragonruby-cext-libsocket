57b923757003391528c927e8d33eb3b81b432b54
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-04-19 13:10:30 +0900

    Use nohup and echo tcp server log

--- .github/workflows/run_tests.sh
+++ .github/workflows/run_tests.sh
@@ -3,14 +3,18 @@
 mkdir -p $TESTGAME_DIR/libsocket
 cp *.rb $TESTGAME_DIR/libsocket
 
-ruby .github/workflows/test-tcp-server.rb &
+nohup ruby .github/workflows/test-tcp-server.rb &> tcp-server.log &
+
+sleep 5
 
 SDL_VIDEODRIVER=dummy SDL_AUDIODRIVER=dummy ./dragonruby $TESTGAME_DIR
 
 dr_output=$(cat $TESTGAME_DIR/dr_output)
 echo "DR Output was: $dr_output"
+echo "TCP Server output was:"
+cat tcp-server.log
 
-if [ $dr_output == "loaded,connected,sent" ]; then
+if [ "$dr_output" == "loaded,connected,sent" ]; then
   echo "Tests finished successfully."
   exit 0
 else
